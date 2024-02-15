d42649738fa080ff7b2f21ced4bc95864af464da
rastydnb <luisramos191@gmail.com>
2024-02-15 20:04:46 +0100

    Agrega recurso helm_release para cilium y actualiza valores.yaml

--- terraform/modules/flux/main.tf
+++ terraform/modules/flux/main.tf
@@ -5,4 +5,20 @@ resource "helm_release" "flux" {
   namespace        = "flux-system"
   create_namespace = true
   values = [fileexists("${path.root}/${var.values_file}") == true ? file("${path.root}/${var.values_file}") : ""]
+  depends_on = [ helm_release.cilium ]
+}
+
+
+
+resource "helm_release" "cilium" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  namespace  = "networking"
+  version    = "1.15.1"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
 }
